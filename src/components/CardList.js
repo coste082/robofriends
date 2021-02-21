f0c0c24436851164442ff67b66529e03659d90bf
Dan Costello <dan.costello2@gmail.com>
2021-02-21 11:48:04 -0600

    update deployment

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  if (true) {
-    throw new Error('noo');
-  }
+  // if (true) {
+  //   throw new Error('noo');
+  // }
   return (
     <div>
       {robots.map((user, i) => {
