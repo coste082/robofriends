0c58aac5089e9dbf87fc6d017a6ca012dd1d2f49
Dan Costello <dan.costello2@gmail.com>
2021-02-21 11:44:58 -0600

    Updates

---
+++ precache-manifest.12501a0d4664504d0b6008fc322252f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5472f54b8d12e60e67ed",
+    "url": "/robofriends/static/css/main.f78320cd.chunk.css"
+  },
+  {
+    "revision": "5472f54b8d12e60e67ed",
+    "url": "/robofriends/static/js/main.b1c0297e.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "401f984087f1f9558e03",
+    "url": "/robofriends/static/css/2.bbd24505.chunk.css"
+  },
+  {
+    "revision": "401f984087f1f9558e03",
+    "url": "/robofriends/static/js/2.971b8cad.chunk.js"
+  },
+  {
+    "revision": "670b058bbe88246884e4c9e2313a396b",
+    "url": "/robofriends/index.html"
+  }
+];
